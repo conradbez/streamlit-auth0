dd0aeed010f4a5081796575c93229125984dbcac
Enxhell <enxhell.luzhnica@gmail.com>
2022-04-25 14:13:15 +0200

    fix(domain): Remove domain checks

--- auth0_component/__init__.py
+++ auth0_component/__init__.py
@@ -21,9 +21,6 @@ from jose import jwt
 
 def getVerifiedSubFromToken(token, domain):
     domain = "https://"+domain
-    if domain[-10:] != '.auth0.com':
-        print('domain should end with ".us.auth0.com" (no slash)')
-        raise ValueError
     jsonurl = urlopen(domain+"/.well-known/jwks.json")
     jwks = json.loads(jsonurl.read())
     unverified_header = jwt.get_unverified_header(token)
